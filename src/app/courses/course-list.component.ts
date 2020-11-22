ce0e88551b5d306d22d63eee5eb1e613a54273db
saldanajg4 <saldanajg4@hotmail.com>
2020-11-21 22:05:56 -0600

    NgRedux implemented

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CourseService } from './course.service';
 import { Course } from './course';
 import { FilterTextComponent, FilterService } from '../blocks/filter-text';
+import {  IAppState } from '../store';//to dispatch action
+import { NgRedux, select } from 'ng2-redux';
+import { Observable } from 'rxjs';
+import { CourseActions } from './course.action';
+// filterCourses(searchText)
 
 @Component({
   selector: 'app-course-list',
@@ -9,26 +14,25 @@ import { FilterTextComponent, FilterService } from '../blocks/filter-text';
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[];
-  filteredCourses = this.courses;
+  //subscribing to the filteredCourses from the store as a stream
+  // so, include | async.  This is a much cleaner, effective way
+  //to declare and subscribe to the store requesting properties from it
+  @select('filteredCourses') filteredCourses$: Observable<Course>
 
-  constructor(private _courseService: CourseService, private _filterService: FilterService) {
-  }
-
+  constructor(private ngRedux: NgRedux<IAppState>,
+    private courseActions: CourseActions){}
+  
+    //now removed the dispatch from the store and add the one 
+    //from courseActions.  Remove the store import and also check
+    // the reducer for the courses course actions
   filterChanged(searchText: string) {
     console.log('user searched: ', searchText);
-    this.filteredCourses = this._filterService.filter(searchText, ['id', 'name', 'topic'], this.courses);
-  }
-
-  getCourses() {
-    this._courseService.getCourses()
-      .subscribe(courses => {
-        this.courses = this.filteredCourses = courses;
-      });
+    // store.dispatch(filterCourses(searchText));
+    this.courseActions.filterCourses(searchText);
   }
 
   ngOnInit() {
-    this.getCourses();
+    this.courseActions.getCourses();
     componentHandler.upgradeDom();
   }
 }
